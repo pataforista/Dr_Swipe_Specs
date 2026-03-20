49ace87b876130d481e4a2b100cc6478bdbb9779
pataforista <cesaraugustocelada@gmail.com>
2026-03-20 13:26:29 -0600

    1

--- dr-swipe/vite.config.ts
+++ dr-swipe/vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Dr_Swipe_Specs/',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
